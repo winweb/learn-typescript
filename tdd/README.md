0252841d5d5664455e4f380511bfa1af17b9a1d7
Junlapong L <junlapong@gmail.com>
2020-05-25 07:08:24 +0700

    add tdd

---
+++ tdd/README.md
@@ -0,0 +1,4 @@
+# TDD
+
+- [Set Up Environment สำหรับ TypeScript](https://link.medium.com/Lo9jp61WH6)
+
---
+++ tdd/test/canary_test.ts
@@ -0,0 +1,8 @@
+import { expect } from "chai"
+
+describe("Canary test", () => {
+    it("should pass this canary test", () => {
+        expect(true).to.equal(true)
+    })
+})
+
