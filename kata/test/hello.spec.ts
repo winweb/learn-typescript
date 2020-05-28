b77b58ee0a9df1b44549f0986661d8a0ef3821be
Junlapong L <junlapong@gmail.com>
2020-05-29 06:36:35 +0700

    fix typo

--- kata/test/hello.spec.ts
+++ kata/test/hello.spec.ts
@@ -1,6 +1,6 @@
 import { sayHello} from "../src/hello";
 
-describe("This is a siy hello", () => {
+describe("This is a say hello", () => {
   test("Check the saayHello function", () => {
     expect(sayHello("Jun")).toEqual("Hello Jun");
   });
